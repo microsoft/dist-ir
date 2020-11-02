1819d18c06917691d3e905d8b4ed5f91aaf5c431
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2020-11-03 00:03:02 +0530

    Fix path in test_mnist to be relative

--- dist_ir/tests/onnx/test_mnist.py
+++ dist_ir/tests/onnx/test_mnist.py
@@ -1,7 +1,8 @@
+from pathlib import Path
+
 import dist_ir
 
-# TODO: Fix this path (command line arg or os.path.dirname?)
-onnx_model_path = "../../../test/mnist_gemm_bw_running.onnx"
+onnx_model_path = Path(__file__).parent / "../../../test/mnist_gemm_bw_running.onnx"
 backend = None
 
 dist_ir.graph.import_from_onnx(onnx_model_path, backend)

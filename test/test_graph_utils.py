1fd7f0dd637148c3512ce5c73a67c25d02719047
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2020-11-03 01:03:42 +0530

    Fix path in test_graph_utils

--- test/test_graph_utils.py
+++ test/test_graph_utils.py
@@ -4,7 +4,7 @@ import dist_ir
 
 
 def test_import_from_onnx():
-    onnx_model_path = Path(__file__).parent / "../../../test/mnist_gemm_bw_running.onnx"
+    onnx_model_path = Path(__file__).parent / "mnist_gemm_bw_running.onnx"
     backend = None
 
     dist_ir.graph.import_from_onnx(onnx_model_path, backend)

42bc730a98758da45774c142025f14ba5d9186f0
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2020-11-06 22:14:22 +0530

    Some fixes in test_sequential_executor

--- test/test_sequential_executor.py
+++ test/test_sequential_executor.py
@@ -3,7 +3,7 @@ import pytest
 import sys
 import torch
 
-from dist_ir.ir import Module, Value
+from dist_ir.ir import Module
 from dist_ir.ir.type import Float, Tensor
 from dist_ir.executor import SequentialExecutor
 
@@ -15,14 +15,14 @@ class Helper:
         self._module = Module()
         if self._backend == "numpy":
             data = np.random.normal(size=(4, 4))
-            self._t1 = self._module.add_input_value(Value("a", Tensor(Float())))
-            self._t2 = self._module.add_input_value(Value("b", Tensor(Float())))
-            self._t3 = self._module.add_input_value(Value("c", Tensor(Float())))
+            self._t1 = self._module.add_input_value("a", Tensor(Float()))
+            self._t2 = self._module.add_input_value("b", Tensor(Float()))
+            self._t3 = self._module.add_input_value("c", Tensor(Float()))
         elif self._backend == "torch":
             data = torch.randn(size=(4, 4))
-            self._t1 = self._module.add_input_value(Value("a", Tensor(Float())))
-            self._t2 = self._module.add_input_value(Value("b", Tensor(Float())))
-            self._t3 = self._module.add_input_value(Value("c", Tensor(Float())))
+            self._t1 = self._module.add_input_value("a", Tensor(Float()))
+            self._t2 = self._module.add_input_value("b", Tensor(Float()))
+            self._t3 = self._module.add_input_value("c", Tensor(Float()))
         else:
             raise ValueError(f"Unknown backend {self._backend}")
         self._input_data = {
@@ -40,7 +40,7 @@ def helper_obj(request):
 
 def test_single_add(helper_obj):
     h = helper_obj
-    h._module.add_op("Add_0", "Add", h._t1, h._t2)
+    h._module.add_op("Add", "Add_0", [h._t1, h._t2])
     outputs = h._executor.compute(h._module, h._input_data)
     result = outputs["Add_0"].data
     if h._backend == "numpy":
@@ -51,8 +51,8 @@ def test_single_add(helper_obj):
 
 def test_double_add(helper_obj):
     h = helper_obj
-    x = h._module.add_node("Add_0", "Add", h._t1, h._t2)
-    h._module.add_node("Add_1", "Add", h._t3, x)
+    x = h._module.add_op("Add", "Add_0", [h._t1, h._t2])
+    h._module.add_op("Add", "Add_1", [h._t3, x])
     outputs = h._executor.compute(h._module, h._input_data)
     result = outputs["Add_1"].data
     if h._backend == "numpy":
@@ -67,8 +67,8 @@ def test_double_add(helper_obj):
 
 def test_double_add_inverted(helper_obj):
     h = helper_obj
-    x = h._module.add_node("Add_0", "Add", h._t1, h._t2)
-    h._module.add_node("Add_1", "Add", x, h._t3)
+    x = h._module.add_op("Add", "Add_0", [h._t1, h._t2])
+    h._module.add_op("Add", "Add_1", [x, h._t3])
     outputs = h._executor.compute(h._module, h._input_data)
     result = outputs["Add_1"].data
     if h._backend == "numpy":
@@ -83,7 +83,7 @@ def test_double_add_inverted(helper_obj):
 
 def test_single_matmul(helper_obj):
     h = helper_obj
-    h._module.add_node("MatMul_0", "MatMul", h._t1, h._t2)
+    h._module.add_op("MatMul", "MatMul_0", [h._t1, h._t2])
     outputs = h._executor.compute(h._module, h._input_data)
     result = outputs["MatMul_0"].data
     if h._backend == "numpy":
@@ -94,8 +94,8 @@ def test_single_matmul(helper_obj):
 
 def test_double_matmul(helper_obj):
     h = helper_obj
-    x = h._module.add_node("MatMul_0", "MatMul", h._t1, h._t2)
-    h._module.add_node("MatMul_1", "MatMul", h._t3, x)
+    x = h._module.add_op("MatMul", "MatMul_0", [h._t1, h._t2])
+    h._module.add_op("MatMul", "MatMul_1", [h._t3, x])
     outputs = h._executor.compute(h._module, h._input_data)
     result = outputs["MatMul_1"].data
     if h._backend == "numpy":
@@ -111,8 +111,8 @@ def test_double_matmul(helper_obj):
 
 def test_double_matmul_inverted(helper_obj):
     h = helper_obj
-    x = h._module.add_node("MatMul_0", "MatMul", h._t1, h._t2)
-    h._module.add_node("MatMul_1", "MatMul", x, h._t3)
+    x = h._module.add_op("MatMul", "MatMul_0", [h._t1, h._t2])
+    h._module.add_op("MatMul", "MatMul_1", [x, h._t3])
     outputs = h._executor.compute(h._module, h._input_data)
     result = outputs["MatMul_1"].data
     if h._backend == "numpy":

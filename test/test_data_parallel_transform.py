53f18fe21f6bf1f6a05796bef138d439c2ff239b
Keshav Santhanam <t-kesan@microsoft.com>
2020-11-15 21:15:03 -0800

    Add test for double matmul

--- test/test_data_parallel_transform.py
+++ test/test_data_parallel_transform.py
@@ -26,5 +26,28 @@ def test_single_matmul():
     print(transformed_module)
 
 
+def test_double_matmul():
+    module = Module()
+
+    a = module.add_input_value("a", Tensor(Float(), (4, 4)))
+    b = module.add_input_value("b", Tensor(Float(), (4, 4)))
+    c = module.add_input_value("c", Tensor(Float(), (4, 4)))
+    x = module.add_op("MatMul", "MatMul0", inputs=[a, b], output_names=["x"])
+    y = module.add_op("MatMul", "MatMul1", inputs=[x, c], output_names=["y"])
+    transform = DataParallelTransform(
+        partitioned_input_name="a", partition_dim=0, num_partitions=2
+    )
+    transformed_module = transform.apply(module)
+    print("-" * 88)
+    print("Original module")
+    print("-" * 88)
+    print(module)
+    print()
+    print("-" * 88)
+    print("Transformed module")
+    print("-" * 88)
+    print(transformed_module)
+
+
 if __name__ == "__main__":
-    test_single_matmul()
+    test_double_matmul()

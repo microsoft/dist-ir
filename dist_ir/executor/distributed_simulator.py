ac35565b7af00ab2bd950ee39ec1ddfe585efbb1
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2020-12-04 17:38:52 +0000

    Simulator generates Chrome trace

--- dist_ir/executor/distributed_simulator.py
+++ dist_ir/executor/distributed_simulator.py
@@ -1,10 +1,9 @@
-from ..ir import Module
-from ..ir.type import Tensor
-from . import utils
-from .shape_inference import ShapeInferenceRegister
-
-import copy
+from copy import deepcopy
 from collections import defaultdict
+import json
+
+from ..ir import Module
+from . import utils
 
 
 class DistributedSimulatorState:
@@ -13,6 +12,32 @@ class DistributedSimulatorState:
         self.consumers = defaultdict(lambda: 0)
         self.peak_memory = defaultdict(lambda: 0.0)
         self.live_memory = defaultdict(lambda: 0.0)
+        self.trace = []
+
+    def add_trace_event(self, op_name, device, start_time, duration):
+        self.trace.append(
+            {
+                "name": op_name,
+                "ph": "X",
+                "ts": start_time,
+                "dur": duration,
+                "pid": 0,
+                "tid": device.device_id,
+            }
+        )
+
+    def dump_chrome_trace(self, fname):
+        # Chrome trace expects times in microseconds
+        # TODO multiplying by a larger num here to make debugging easier
+        _trace = deepcopy(self.trace)
+        for event in _trace:
+            event["ts"] = int(event["ts"] * 1e11)
+            event["dur"] = int(event["dur"] * 1e11)
+            # event["ts"] = event["ts"] * 1e6
+            # event["dur"] = event["dur"] * 1e6
+
+        with open(fname, "w") as fout:
+            json.dump(_trace, fout, indent=0)
 
 
 class DistributedSimulator:
@@ -25,7 +50,7 @@ class DistributedSimulator:
             in_edges = op.get_in_edges()
             out_edges = op.get_out_edges()
 
-            # Synchronize all input and output devices for this op if necessary.
+            # Synchronize all input and output devices for this op.
             input_devices = utils.get_all_devices(in_edges)
             output_devices = utils.get_all_devices(out_edges)
             input_and_output_devices = input_devices.union(output_devices)
@@ -44,7 +69,18 @@ class DistributedSimulator:
                 submodule_state = DistributedSimulatorState()
                 self._simulate(submodule, submodule_state)
                 device_vars = submodule_state.timestamps.keys()
+                assert len(device_vars) == 1
+                # TODO what happens when pmaps are nested?
                 bound_devices = op.get_attribute("devices")
+                # Add submodule's trace to trace of all participating devices
+                for device in bound_devices:
+                    for event in submodule_state.trace:
+                        # Need to add pmap's starting timestamp to event
+                        # since submodule_state started at time 0
+                        start_time = event["ts"] + state.timestamps[device]
+                        state.add_trace_event(
+                            event["name"], device, start_time, event["dur"]
+                        )
                 for device_var in device_vars:
                     for bound_device in bound_devices:
                         state.timestamps[bound_device] += submodule_state.timestamps[
@@ -60,6 +96,12 @@ class DistributedSimulator:
             else:
                 costs = self._cost_model.infer_costs(op)
                 for device in costs:
+                    state.add_trace_event(
+                        op_name,
+                        device,
+                        state.timestamps[device],
+                        costs[device],
+                    )
                     state.timestamps[device] += costs[device]
 
             # Update the live memory.

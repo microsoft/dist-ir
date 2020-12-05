b2c04ccc1955d2372f910755617a540af40e9cc2
Keshav Santhanam <keshav2@stanford.edu>
2020-12-04 17:21:45 -0800

    Add Jupyter notebook for initial results (#11)

--- dist_ir/executor/cost_inference.py
+++ dist_ir/executor/cost_inference.py
@@ -1,5 +1,6 @@
 from . import utils
 
+import numpy as np
 
 BYTES_IN_GB = 8.0e9
 
@@ -16,18 +17,41 @@ class CostModel:
         self._op_register = {
             "Allreduce": self._infer_costs_for_allreduce,
             "Broadcast": self._infer_costs_for_broadcast_scatter,
+            "Concat": self._infer_costs_for_concat,
             "Gather": self._infer_costs_for_gather,
+            "Loss": self._infer_costs_for_loss,
+            "LossGrad": self._infer_costs_for_loss_grad,
             "MatMul": self._infer_costs_for_matmul,
+            "MatMulGrad": self._infer_costs_for_matmul_grad,
             "Scatter": self._infer_costs_for_broadcast_scatter,
         }
 
     def _infer_costs_for_allreduce(self, op, inputs, outputs):
         costs = {}
-        output_devices = utils.get_all_devices(outputs)
-        for device in output_devices:
+        input_size = inputs[0].type.types[0].size()
+        devices = list(utils.get_all_devices(outputs))
+        num_devices = len(devices)
+        per_device_data = 2 * input_size * (num_devices - 1) / num_devices
+        per_device_data_gb = per_device_data / BYTES_IN_GB
+        all_bandwidths = []
+        for i in range(len(devices)):
+            for j in range(i, len(devices)):
+                all_bandwidths.append(
+                    self._topology.get_bandwidth(devices[i], devices[j])
+                )
+        average_bandwidth = np.mean(all_bandwidths)
+        cost = per_device_data_gb / average_bandwidth
+        for device in devices:
+            costs[device] = cost
+
+        return costs
+
+    def _infer_costs_for_concat(self, op, inputs, outputs):
+        costs = {}
+        input_devices = utils.get_all_devices(inputs)
+        for device in input_devices:
             # TODO: Compute cost properly
             costs[device] = 0
-
         return costs
 
     def _infer_costs_for_gather(self, op, inputs, outputs):
@@ -37,6 +61,22 @@ class CostModel:
             # TODO: Compute cost properly
             costs[device] = 0
 
+        return costs
+
+    def _infer_costs_for_loss(self, op, inputs, outputs):
+        costs = {}
+        input_devices = utils.get_all_devices(inputs)
+        for device in input_devices:
+            # TODO: Compute cost properly
+            costs[device] = 0
+        return costs
+
+    def _infer_costs_for_loss_grad(self, op, inputs, outputs):
+        costs = {}
+        input_devices = utils.get_all_devices(inputs)
+        for device in input_devices:
+            # TODO: Compute cost properly
+            costs[device] = 0
         return costs
 
     def _infer_costs_for_matmul(self, op, inputs, outputs):
@@ -49,6 +89,13 @@ class CostModel:
         # TODO: Use a better way of computing runtime from FLOPs
         runtime = flops / self._device_speeds[device.device_type]
         return {device: runtime}
+
+    def _infer_costs_for_matmul_grad(self, op, inputs, outputs):
+        costs = self._infer_costs_for_matmul(op, [inputs[0], inputs[1]], [outputs[0]])
+        # TODO: Replace with more accurate estimate.
+        for device in costs:
+            costs[device] *= 2
+        return costs
 
     def _infer_costs_for_broadcast_scatter(self, op, inputs, outputs):
         costs = {}

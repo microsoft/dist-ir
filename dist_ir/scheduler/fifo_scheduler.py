ff6f43ef568e0f17064c7d4ea44bf050506e22d6
Keshav Santhanam <t-kesan@microsoft.com>
2020-12-08 17:22:52 -0800

    Update FIFO scheduling algorithm and results notebook

--- dist_ir/scheduler/fifo_scheduler.py
+++ dist_ir/scheduler/fifo_scheduler.py
@@ -6,5 +6,7 @@ from .scheduler import Scheduler
 
 class FIFOScheduler(Scheduler):
     def _get_next_op_to_schedule(self, device: Device) -> Tuple[str, int]:
-        ready_ops = sorted(self._ready_ops[device], key=lambda x: x[1])
-        return ready_ops[0]
+        return self._ready_ops[device][0]
+
+        # ready_ops = sorted(self._ready_ops[device], key=lambda x: x[1])
+        # return ready_ops[0]

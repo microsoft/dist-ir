647c68a46e3ec46408164aa71b18f3c28c31d4f4
Keshav Santhanam <keshav2@stanford.edu>
2021-01-07 22:28:20 -0800

    Add H -> D test

--- test/test_composable_transforms.py
+++ test/test_composable_transforms.py
@@ -105,5 +105,89 @@ def test_data_parallelism_followed_by_horizontal_parallelism():
     np.testing.assert_array_almost_equal(y, ref)
 
 
+def test_horizontal_parallelism_followed_by_data_parallelism():
+    h = Helper()
+    xs = h.function.add_op(
+        "Broadcast",
+        "Broadcast/x",
+        inputs=[h.x],
+        attributes={"devices": h.devices[1:]},
+        output_names=["xs"],
+    )
+    wAs = h.function.add_op(
+        "Scatter",
+        "Scatter/wA",
+        inputs=[h.wA],
+        attributes={"dim": 1, "devices": h.devices[1:]},
+        output_names=["wAs"],
+    )
+    wBs = h.function.add_op(
+        "Broadcast",
+        "Broadcast/wB",
+        inputs=[h.wB],
+        attributes={"devices": h.devices[1:]},
+        output_names=["wBs"],
+    )
+    a_subfunction = FunctionMaker()
+    a_subfunction.inputs.append(h.x)
+    a_subfunction.inputs.append(h.wA)
+    ai = a_subfunction.add_op(
+        "MatMul", "MatMul/a", inputs=[h.x, h.wA], output_names=["a"]
+    )
+    a_subfunction = a_subfunction.finalize()
+    ais = h.function.add_op(
+        "Pmap",
+        "Pmap/a",
+        inputs=[xs, wAs],
+        subfunctions=[a_subfunction],
+        attributes={"devices": h.devices[1:]},
+        output_names=["ais"],
+    )
+    a = h.function.add_op(
+        "Gather",
+        "Gather/ais",
+        inputs=[ais],
+        attributes={"dim": 1, "device": h.devices[0]},
+        output_names=["a"],
+    )
+    as_ = h.function.add_op(
+        "Scatter",
+        "Scatter/a",
+        inputs=[a],
+        attributes={"dim": 0, "devices": h.devices[1:]},
+        output_names=["as"],
+    )
+    y_subfunction = FunctionMaker()
+    y_subfunction.inputs.append(ai)
+    y_subfunction.inputs.append(h.wB)
+    yi = y_subfunction.add_op(
+        "MatMul", "MatMul/y", inputs=[ai, h.wB], output_names=["y"]
+    )
+    y_subfunction = y_subfunction.finalize()
+    yis = h.function.add_op(
+        "Pmap",
+        "Pmap/y",
+        inputs=[as_, wBs],
+        subfunctions=[a_subfunction],
+        attributes={"devices": h.devices[1:]},
+        output_names=["yis"],
+    )
+    y = h.function.add_op(
+        "Gather",
+        "Gather/y",
+        inputs=[yis],
+        attributes={"dim": 0, "device": h.devices[0]},
+        output_names=["y"],
+    )
+    h.function = h.function.finalize()
+    output_data = h.executor.compute(h.function, h.input_data)
+    y = output_data[y]
+    ref = np.matmul(
+        np.matmul(h.input_data[h.x], h.input_data[h.wA]), h.input_data[h.wB]
+    )
+    np.testing.assert_array_almost_equal(y, ref)
+
+
 if __name__ == "__main__":
     test_data_parallelism_followed_by_horizontal_parallelism()
+    test_horizontal_parallelism_followed_by_data_parallelism()

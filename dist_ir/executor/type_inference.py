f83cd3a8fdefb83aeed425bee7b3f13f2380a0bb
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2021-01-12 18:02:20 +0000

    Implement pmap's type prop fn, fix some others

--- dist_ir/executor/type_inference.py
+++ dist_ir/executor/type_inference.py
@@ -2,25 +2,46 @@
 A type inference module that converts an untyped DistIR Function into one where
 every Value is typed with shape and dtype information, given input types or
 example inputs.
+
+Type inference requires a register mapping ops to type propagation functions:
+- This is a function foo(op, x1, x2, .., xN), where op is an N-ary Op, and x1 to
+    xN are Types of the inputs.
+- The function should check that the inputs have the expected types.
+- The function should return the type of the output/a tuple of types of the
+    outputs.
+(When we say types we also mean shape and device information.)
 """
 
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 from ..ir import Function, FunctionMaker, Op, Value
 from ..ir.type import Type, Tensor, TupleType
 
 
+def _raise_type_error(op, *args):
+    raise ValueError(f"Type error: op\n{op}\nwas given arguments\n{tuple(args)}")
+
+
 def _allreduce_prop_fn(op, x):
-    assert isinstance(x, TupleType)
+    devices = tuple(t.device for t in x.types)
+    if not (
+        isinstance(x, TupleType)
+        and all(isinstance(t, Tensor) for t in x.types)
+        and len(x.types) > 0
+        and all(t.shape == x.types[0].shape for t in x.types)
+        and len(set(devices)) == len(devices)
+    ):
+        _raise_type_error(op, x)
     return x
+
+
+# TODO update the below prop functions to be as robust as _allreduce_prop_fn
 
 
 def _broadcast_prop_fn(op, x):
     assert isinstance(x, Tensor)
     devices = op.attributes["devices"]
-    types = []
-    for device in devices:
-        types.append(Tensor(dtype=x.dtype, shape=x.shape, device=device))
+    types = (Tensor(dtype=x.dtype, shape=x.shape, device=device) for device in devices)
     return TupleType(tuple(types))
 
 
@@ -37,8 +58,14 @@ def _concat_prop_fn(op, x, y):
 
 
 def _elementwise_tensor_op_prop_fn(op, x, y):
-    assert isinstance(x, Tensor) and isinstance(y, Tensor)
-    assert x.dtype == y.dtype and x.shape == y.shape and x.device == y.device
+    if not (
+        isinstance(x, Tensor)
+        and isinstance(y, Tensor)
+        and x.dtype == y.dtype
+        and x.shape == y.shape
+        and x.device == y.device
+    ):
+        _raise_type_error(op, x, y)
     return x
 
 
@@ -60,9 +87,14 @@ def _gather_prop_fn(op, x):
 
 
 def _matmul_prop_fn(op, x, y):
-    assert isinstance(x, Tensor) and isinstance(y, Tensor)
-    assert x.dtype == y.dtype and x.device == y.device
-    assert x.shape[1] == y.shape[0]
+    if not (
+        isinstance(x, Tensor)
+        and isinstance(y, Tensor)
+        and x.dtype == y.dtype
+        and x.device == y.device
+        and x.shape[1] == y.shape[0]
+    ):
+        _raise_type_error(op, x, y)
     return Tensor(dtype=x.dtype, shape=(x.shape[0], y.shape[1]), device=x.device)
 
 
@@ -131,13 +163,53 @@ TypePropRegister = {
     "Send": _send_prop_fn,
     "Split": _split_prop_fn,
 }
-"""
-Type propagation functions:
-For each op, a function that returns the types of the outputs of the op,
-given the original op and a list of typed input Values.
-When we say types we also mean shape and device information.
-These functions also perform type checking: that inputs have expected types.
-"""
+
+# Handling pmap specially for now since it needs to return a typed subfunction
+
+
+def _pmap_prop_fn(op: Op, input_types: Tuple[Type]):
+    """pmap maps over a tuple of values, all of the same type and shape, but on
+    distinct devices. For convenience, to avoid a lot of zipping, we allow
+    multiple inputs, as long as they are all tuples of the same length and the
+    list of devices in each tuple are exactly the same.
+    """
+    # Pmap expects 1 or more tuples as input
+    assert isinstance(input_types, tuple) and len(input_types) > 0
+    assert all(isinstance(t, TupleType) for t in input_types)
+    # Check that pmap's arguments all have same length and shapes
+    assert len(set(len(t.types) for t in input_types)) == 1
+    for t in input_types:
+        assert all(isinstance(x, Tensor) for x in t.types)
+        assert len(set(x.shape for x in t.types)) == 1
+        assert len(set(x.dtype for x in t.types)) == 1
+    # Check that pmap's arguments are on distinct devices
+    devices = tuple(x.device for x in input_types[0].types)
+    assert len(set(devices)) == len(devices)
+    # Check that all inputs have same list of devices
+    for t in input_types:
+        assert devices == tuple(x.device for x in t.types)
+
+    # Subfunction's inputs are given by pmap's arguments, but on device d
+    subfn_inputs = [
+        Value(v.name, t.types[0])
+        for v, t in zip(op.subfunctions[0].inputs, input_types)
+    ]
+
+    # Recursively call infer_types on subfunction
+    assert len(op.subfunctions) == 1
+    subfunctions = [infer_types(op.subfunctions[0], subfn_inputs)]
+
+    # Pmap's output types are given by subfunction's output types
+    out_types = tuple(
+        TupleType(
+            tuple(
+                Tensor(shape=t.type.shape, dtype=t.type.dtype, device=d)
+                for d in devices
+            )
+        )
+        for t in subfunctions[0].outputs
+    )
+    return out_types, subfunctions
 
 
 def infer_types(function: Function, inputs: List[Value]) -> Function:
@@ -172,10 +244,7 @@ def infer_types(function: Function, inputs: List[Value]) -> Function:
 
         # Infer types of outputs and create output values
         if op.op_type == "Pmap":
-            # TODO: Propagate device variables here?
-            for inp in op.subfunctions[0].inputs:
-                assert_is_typed(inp)
-            subfunctions = [infer_types(op.subfunctions[0], op.subfunctions[0].inputs)]
+            out_types, subfunctions = _pmap_prop_fn(op, input_types)
         else:
             out_types = TypePropRegister[op.op_type](op, *input_types)
             if not isinstance(out_types, tuple):

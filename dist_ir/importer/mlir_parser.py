ff1af7a85df020db84f52f6e5dc8841fec395902
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2021-01-27 23:13:33 +0000

    MLIR parser: support integer type

--- dist_ir/importer/mlir_parser.py
+++ dist_ir/importer/mlir_parser.py
@@ -6,7 +6,7 @@ from typing import Any, Dict, List, Union
 import mlir
 from ..ir import Function, FunctionMaker, Value
 from ..ir.device import Device
-from ..ir.type import Float, Tensor
+from ..ir.type import Float, Int, Tensor
 
 
 @dataclass
@@ -44,7 +44,7 @@ def _get_device(d: Union[int, str], context: Context) -> Device:
 
 def _parse_type(mlir_type, context: Context):
     # Unfortunately, I can't inspect the MLIR type object, so parsing the string:
-    dtype_map = {"f32": Float()}
+    dtype_map = {"f32": Float(), "i64": Int()}
 
     def parse_shape_dtype(shape_str):
         dims = shape_str.strip().split("x")

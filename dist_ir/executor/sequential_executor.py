513e9ed5a34ed5b78b59c075b539df6209fbc8b3
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2021-03-15 13:19:48 +0000

    Remove redundant code in SequentialExecutor

--- dist_ir/executor/sequential_executor.py
+++ dist_ir/executor/sequential_executor.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Sequence
+from typing import Any, Sequence, Tuple
 
 from .absint import AbstractInterpreter, convert_impls_to_semantics
 from .backend_register import BackendRegister
@@ -12,34 +12,7 @@ class SequentialExecutor:
         semantics = convert_impls_to_semantics(BackendRegister[backend])
         self.interpreter = AbstractInterpreter(semantics=semantics)
 
-    def _compute_op(self, op: Op, inputs: List[Any]):
-        """Executes the given op and returns its outputs."""
-        op_type = op.op_type
-        if op_type == "Pmap":
-            # Zip the inputs so that we map over each corresponding value
-            inputs = zip(*inputs)
-            # Iterate over the inputs
-            results = []
-            for inps in inputs:
-                # Execute subfunction with appropriate inputs
-                outs = self.compute(op.subfunctions[0], inps)
-                # Match output names to output data using the function output order.
-                ordered_outs = [outs[e] for e in op.subfunctions[0].outputs]
-                results.append(ordered_outs)
-            # Unzip the results
-            results = tuple(zip(*results))
-            return results
-        if op_type not in BackendRegister[self._backend]:
-            raise NotImplementedError(
-                f"No {self._backend} implementation found for op {op_type}"
-            )
-        impl = BackendRegister[self._backend][op_type]
-        output_data = impl(op, inputs)
-        if not isinstance(output_data, tuple):
-            output_data = (output_data,)
-        return output_data
-
-    def compute(self, function: Function, inputs: Sequence[Any]) -> Dict[Value, Any]:
+    def compute(self, function: Function, inputs: Sequence[Any]) -> Tuple[Any]:
         """Executes the function given the specified inputs and returns the final result.
 
         Args:
@@ -47,7 +20,7 @@ class SequentialExecutor:
           inputs: A sequence of input data represented in the specified backend.
 
         Returns:
-          A map from output value to output data.
+          A tuple of outputs.
         """
         state = self.interpreter.interpret(function, inputs)
         return tuple(state.env[v] for v in function.outputs)

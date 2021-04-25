d2f9e0a84064309408cf9fe1d22fa41c28776fc7
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2021-04-25 15:08:34 +0100

    A distributed PyTorch backend

---
+++ dist_ir/backend/torch.py
@@ -0,0 +1,98 @@
+import os
+from tempfile import mkdtemp
+from typing import Any, Tuple
+
+import torch
+import torch.distributed as dist
+from torch import fx
+from torch.multiprocessing import Process
+
+from ..ir import Function
+
+
+# TODO at op creation time, enforce MPIAllgather ops attributes
+def _allgather(x_i, world_size=None, dim=0):
+    xs = [torch.zeros_like(x_i) for _ in range(world_size)]
+    dist.all_gather(xs, x_i)
+    x = torch.cat(xs, dim=dim)
+    return x
+
+
+_op_to_torch = {
+    "MatMul": torch.matmul,
+    "MPIAllgather": _allgather,
+}
+
+
+def function_to_module(fn: Function) -> torch.nn.Module:
+    g = fx.Graph()
+    value_map = {}
+
+    # TODO need to check that fn has unique value names
+
+    # Convert inputs
+    for v in fn.inputs:
+        value_map[v] = g.placeholder(v.name)
+
+    # Convert ops
+    for op in fn.ops:
+        inputs = tuple(value_map[v] for v in op.inputs)
+        assert len(op.outputs) == 1, "TODO how to handle multiple outputs in fx"
+        kwargs = None if op.attributes is None else {**op.attributes}
+        output = g.call_function(_op_to_torch[op.op_type], inputs, kwargs)
+        value_map[op.outputs[0]] = output
+
+    # Convert outputs
+    for v in fn.outputs:
+        g.output(value_map[v])
+
+    return fx.GraphModule({}, g)
+
+
+def _init_process(rank, size, module, io_dir, backend):
+    """ Initialize the distributed environment. """
+    os.environ["MASTER_ADDR"] = "127.0.0.1"
+    os.environ["MASTER_PORT"] = "29500"
+    dist.init_process_group(backend, rank=rank, world_size=size)
+
+    per_rank_inputs = torch.load(os.path.join(io_dir, f"in.{rank}.pt"))
+
+    # TODO time the next line only
+    res = module(*per_rank_inputs)
+
+    torch.save(res, os.path.join(io_dir, f"out.{rank}.pt"))
+
+
+def run_multiprocesses(
+    per_rank_modules: Tuple[torch.nn.Module],
+    per_rank_inputs: Tuple[Any],
+    backend="gloo",
+):
+    assert len(per_rank_modules) == len(per_rank_inputs)
+    world_size = len(per_rank_modules)
+
+    io_dir = mkdtemp()
+    print("run_multiprocess: saving I/O to:", io_dir)
+    # Save inputs for each per-rank function:
+    # TODO lowered pytorch file numbers devices 0...num_devices-1
+    for d, inps in enumerate(per_rank_inputs):
+        torch.save(inps, os.path.join(io_dir, f"in.{d}.pt"))
+
+    processes = []
+    for rank, per_rank_module in enumerate(per_rank_modules):
+        p = Process(
+            target=_init_process,
+            args=(rank, world_size, per_rank_module, io_dir, backend),
+        )
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+    # Load outputs:
+    per_rank_outputs = [
+        torch.load(os.path.join(io_dir, f"out.{d}.pt")) for d in range(world_size)
+    ]
+
+    return per_rank_outputs

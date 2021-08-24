a8d629b627b4d387fccf5114643cec1e1e3d16e7
Keshav Santhanam <keshav2@stanford.edu>
2021-08-24 01:01:20 -0700

    Add examples/calibrate_simulator.py

---
+++ examples/calibrate_simulator.py
@@ -0,0 +1,25 @@
+from dist_ir.executor import (
+    calibrate_device_parameters,
+    calibrate_network_bandwidth,
+    network_bandwidth_debug,
+)
+
+
+def main():
+    """
+    (
+        dram_bandwidth,
+        device_throughput,
+        kernel_launch_overhead,
+    ) = calibrate_device_parameters()
+    print(f"Device throughput: {device_throughput:e}")
+    print(f"DRAM bandwidth: {dram_bandwidth:.2e}")
+    print(f"Kernel launch overhead: {kernel_launch_overhead}")
+    network_bandwidth = calibrate_network_bandwidth()
+    print(f"Network bandwidth: {network_bandwidth}")
+    """
+    network_bandwidth_debug()
+
+
+if __name__ == "__main__":
+    main()
